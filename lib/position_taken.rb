f319dfb624eb966bac5ea53680b8885aa851fe7c
pacosta613 <pedro.acosta613@gmail.com>
2015-11-08 00:01:06 -0500

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,8 @@
-def position_taken
-  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-  position = 0
+def position_taken?(board,position)
+
+  if board[position]=="" || board[position] ==" " || board[position] == nil
+    return false
+  else board[position] == "X" || board[position] == "O"
+    return true
+    end
 end
